a7919931462c610a8a8ddbfedeba04e2f751a79f
Gosha <popov.goshan@mail.ru>
2026-09-05 17:09:30 +0300

    Fix

--- app/config.py
+++ app/config.py
@@ -2,10 +2,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str
+    postgres_host: str
+    postgres_port: int
+    postgres_database_name: str
+    postgres_username: str
+    postgres_password: str
+
     api_key_lms: str
     events_provider_url: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    @property
+    def database_url(self) -> str:
+        return (
+            "postgresql+asyncpg://"
+            f"{self.postgres_username}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}"
+            f"/{self.postgres_database_name}"
+        )
 
 
 settings = Settings()
